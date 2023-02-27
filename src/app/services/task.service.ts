cd04557347472815d613c8aab9238b4a7abf5b31
noomdalv <vladlunasan@gmail.com>
2023-02-26 23:23:53 -0500

    installed json-server and added http module

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
@@ -7,10 +8,11 @@ import { TASKS } from '../mock-tasks';
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl: string = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
