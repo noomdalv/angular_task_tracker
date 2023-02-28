aa0d0aba76462707a65fb5bf325d60b2fdc59e80
noomdalv <vladlunasan@gmail.com>
2023-02-28 04:33:29 -0500

    created addTask service and component

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -30,4 +30,8 @@ export class TaskService {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.patch<Task>(url, { reminder: task.reminder }, httpOptions);
   }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
 }
